9a7e9ff07d8d30f8b78c5db9bbceac2dde23351f
pjlkaw <pedrorochabispo@gmail.com>
2026-09-18 19:51:12 -0300

    Interações com Arrays

---
+++ src/Monster.java
@@ -0,0 +1,6 @@
+public class Monster {
+    int id;
+    String nome = "Esqueleto";
+    int vida = 30;
+    boolean vivo; 
+}
--- src/game.java
+++ src/game.java
@@ -1,30 +1,48 @@
 import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthStyle;
+
+import java.util.ArrayList; 
+
 public class game {
     public static void main(String[] args) throws Exception {
         Scanner teclado = new Scanner(System.in);
+        ArrayList<Player> playersArray = new ArrayList<>(); 
 
+        Player player = new Player();
         while (true) {
     
-            Player player = new Player();
-            player.vida = 100;
+            player.vida = 0;
     
             System.out.print("Nome do player: ");
             String nome = teclado.nextLine();
             
             player.nome = nome;
+
+            String playerEstado = "vivo";
+
+            if (player.vida == 0) {
+                playerEstado = "morto";
+            }
             
             if (!nome.isEmpty()) {
                 System.out.println("\nPlayer Criado!");
                 System.out.println(String.format("bem vindo, %s", player.nome));
-                System.out.println(String.format("STATS \n Vida: %d Estado: %s",player.vida, player.nome));
+                System.out.println(String.format("STATS \n Vida: %d Estado: %s",player.vida, playerEstado));
             }
+
+                    
+            playersArray.add(player);
+            System.out.println(playersArray);
+
+            Player playerAtual = playersArray.get(0);
+            System.out.println(playerAtual.nome);
             
             if (nome.isEmpty()) {
                 System.out.print("Cancelando.. ");
                 break;
             }
         }
-
 
         teclado.close();
     }

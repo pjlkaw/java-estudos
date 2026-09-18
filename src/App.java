6a6ad615fda97de80732e2f9526122a521cb80ab
unknown <pedrorochabispo@gmail.com>
2026-09-18 01:21:14 -0300

    tipos de retornos de função

--- src/App.java
+++ src/App.java
@@ -4,22 +4,32 @@ public class App {
         
         String mensagemFalse = "não ta";
 
-        
         for ( int i = 0; i<=5; i++) {
-            System.out.print(mensagemFalse);
+            System.out.println(mensagemFalse);
             if (i == 5) {
                 ativo = true;
             }
         }
         if (ativo == true) {
             mensagem("ta ativo");
-            ativo = false;
-            System.out.print("voltou pra falso");
+            ativo = retornafalse();
         }
+
+        System.out.println(retornaconta(1, 2));
     }
 
     static void mensagem(String msg) {
         String mensagemTrue = msg;
-        System.out.print(mensagemTrue);
+        System.out.println(mensagemTrue);
+    }
+
+    static int retornaconta(int v1, int v2) {
+        int conta = v1 + v2;
+        return conta;
+    }
+
+    static boolean retornafalse() {
+        System.out.println("voltou pra falso");
+        return false;
     }
 }

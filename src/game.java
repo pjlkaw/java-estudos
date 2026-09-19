f928d6e43023b1f4a9785854897895c4fead8b54
unknown <pedrorochabispo@gmail.com>
2026-09-19 00:32:55 -0300

    menu

--- src/game.java
+++ src/game.java
@@ -1,7 +1,4 @@
 import java.util.Scanner;
-
-import javax.swing.plaf.synth.SynthStyle;
-
 import java.util.ArrayList; 
 
 public class game {
@@ -9,40 +6,54 @@ public class game {
         Scanner teclado = new Scanner(System.in);
         ArrayList<Player> playersArray = new ArrayList<>(); 
 
-        Player player = new Player();
+        
         while (true) {
-    
-            player.vida = 0;
-    
-            System.out.print("Nome do player: ");
-            String nome = teclado.nextLine();
+
+            System.out.println("===== GAME =====");
+            System.out.println("1 - Criar player");
+            System.out.println("2 - Listar players");
+            System.out.println("3 - Ver player");
+            System.out.println("4 - Sair");
+
+            System.out.print("Escolha: ");
+            int opcao = teclado.nextInt();
+            teclado.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    Player player = new Player();
+                    player.vida = 0;
             
-            player.nome = nome;
+                    System.out.print("Nome do player: ");
+                    String nome = teclado.nextLine();
+                    
+                    player.nome = nome;
 
-            String playerEstado = "vivo";
+                    String playerEstado = "vivo";
 
-            if (player.vida == 0) {
-                playerEstado = "morto";
-            }
+                    if (player.vida == 0) {
+                        playerEstado = "morto";
+                    }
+                    
+                    if (!nome.isEmpty()) {
+                        System.out.println("\nPlayer Criado!");
+                        System.out.println(String.format("bem vindo, %s", player.nome));
+                        System.out.println(String.format("STATS \n Vida: %d Estado: %s",player.vida, playerEstado));
+                    }
+
+                    playersArray.add(player);
+                    System.out.println(playersArray);
+
+                    Player playerAtual = playersArray.get(0);
+                    System.out.println(playerAtual.nome);
+                    
+
+                    break;
             
-            if (!nome.isEmpty()) {
-                System.out.println("\nPlayer Criado!");
-                System.out.println(String.format("bem vindo, %s", player.nome));
-                System.out.println(String.format("STATS \n Vida: %d Estado: %s",player.vida, playerEstado));
-            }
-
-            playersArray.add(player);
-            System.out.println(playersArray);
-
-            Player playerAtual = playersArray.get(0);
-            System.out.println(playerAtual.nome);
-            
-            if (nome.isEmpty()) {
-                System.out.print("Cancelando.. ");
-                break;
+                default:
+                    teclado.close();
+                    break;
             }
         }
-
-        teclado.close();
     }
 }
